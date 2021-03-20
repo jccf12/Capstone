61b8236b6ef2bf5bec00a050c43e4f460c23adc9
Juan Castro <jccf.12@hotmail.com>
2021-03-19 19:28:29 -0700

    typo fixes

--- two_file_app/ui.R
+++ two_file_app/ui.R
@@ -20,15 +20,15 @@ ui <- fluidPage(
                 style="text-align:center;line-height:1.6"),
              style="color:white;background-color:#02A4D3;padding:15px;border-radius:10px"),
           br(),
-          "It is possible that you have been curious about investing in the stock market, but you never really know how it works. 
-          Maybe you do have an idea how it works but you don't know if it's for you. Or maybe you are already an expert on it, and are already excited
+          "It is possible that you have been curious about investing in the stock market, but you never really knew how it works. 
+          Maybe you do have an idea how it works but you don't know if it's for you. Or maybe you are already an expert on it, and are excited
           about predicting the stock market, and you want to know what tools are out there that can help you with it. Whatever is the case for you, I hope that this site 
           will be somehow useful for you.",
           h2("What you will find here"),
           column(
             div(
               h4("Analysis"),
-              p("In this tab, you will be able to explore up to 50 different of the biggest companies in Mexico and United States."),
+              p("In this tab, you will be able to explore up to 50 of the biggest companies in Mexico and United States."),
               p("You can look at the value of each their shares (which refer to a small fraction of the company) and how it was evolved through time."),
               p("You will also be able to test two popular tools that have been used to make predictions of these share prices and compare them."),
               style="color:black;background-color:lavender;padding:15px;border-radius:10px"
@@ -41,9 +41,9 @@ ui <- fluidPage(
               h4("Portfolio"),
               p("Here you will have an opportunity to create your own virtual portfolio ! A hypothetical one, of course, which means that you won't need to 
               actually invest any money."),
-              p("You will be able to go 'back to the past' and see what would have happened if you had invested on some of these 50 companies. Would you have made money?
+              p("You will be able to go 'back in time' and see what would have happened if you had invested on some of these 50 companies. Would you have made money?
               Would you have become rich? Or maybe you lost it all... Let's find out!"),
-              p("You can also use the tools that you explored in the Analysis tab to make predictions into the future! Again you can create whichever hypothetical porfolio
+              p("You can also use the tools that you explored in the Analysis tab to make predictions into the future! You can create whichever hypothetical porfolio
               you like."),
               style="color:black;background-color:lavender;padding:15px;border-radius:10px"
             ),
@@ -54,7 +54,7 @@ ui <- fluidPage(
             div(
               h4("About"),
               p("On this page you can find more information about what happens 'under the hood' to generate the interactive graphs on the Analysis and Portfolio tabs."),
-              p("If you have time, I would suggest checking this page out before you dive into looking at the data and simulating portfolios."),
+              p("If you have time, I would suggest checking out this page before you dive into looking at the data and simulating portfolios."),
               style="color:black;background-color:lavender;padding:15px;border-radius:10px"
             ),
             width = 4,
@@ -115,11 +115,11 @@ ui <- fluidPage(
               predicted values)", 
               style = "font-size:1.1em;line-height: 1.8"),
             div(tags$ul("Thing to focus on:",
-                        tags$li("How has the price evolved for each of the selected stocks? Does it seem to go up and down a lot? Does is have any sharp increase
+                        tags$li("How has the price evolved for each of the selected stocks? Does it seem to go up and down a lot? Does it have any sharp increase
                                 or decrease?"),
-                        tags$li("How well does each the models predict what actually happened. Did the prediction agree about the portfolio going up or down? 
+                        tags$li("How well does each the models predict what actually happened? Do predictions agree about the portfolio going up or down? 
                                 Is the actual value of the price within the margins of error of the prediction?"),
-                        tags$li("What could have affected bad or good predictions? Was there an important world event that might have influence the price (e.g., a
+                        tags$li("What could have affected good or bad predictions? Was there any important world event that might have influence the price (for example, a
                                 global pandemic)?")
                         ),
                 style="text-align:justify;color:white;background-color:#02A4D3;padding:20px;border-radius:7px;font-size:1.2em;line-height: 1.8"),
@@ -142,7 +142,7 @@ ui <- fluidPage(
       sidebarPanel(
         helpText("Choose a pre-defined portfolio composition based on the desired risk level or manually choose stocks you wish to be in your portfolio."),
         helpText("The risk level of a stock is assessed based on their anual volatility and the categorization in 'Low',
-                 'Medium','High' is based Stockopedia Risk Ratings"),
+                 'Medium', 'High' is based Stockopedia Risk Ratings."),
         p(tags$a(href="https://help.stockopedia.com/product-guide/stockranks/advanced/the-riskratings/", 
                  "Stockopedia Risk Ratings")),
         selectInput('risk_level', 'Risk level', risk_levels),
@@ -219,8 +219,7 @@ ui <- fluidPage(
                        of this application is to expand your horizon of financial options. You might, upon learning more about the stock market through the app,
                        decide that the stock market is not for you, and that is also completely fine.\n 
                        My only goal is for you to", em("learn"), "and, based on your learnings, decide
-                       what works best for you, financially speaking. This app does is not meant to be taken as financial advise and I
-                       am not responsible for the financial decisions you make.",
+                       what works best for you, financially speaking. This app is NOT meant to be taken as financial advise.",
                        style="text-align:justify;color:black;background-color:papayawhip;padding:30px;border-radius:10px"),
                      width=8),
                    column(
@@ -240,18 +239,18 @@ ui <- fluidPage(
                 of a table that shows the current price of the stock and the % change in the last day, which means by what percentage the price
                 of the stock changed in the last day. For example, if Apple's stock was valued at $100 yesterday, and today is valued at $102,
                 the % change will show 2%, because the increase of $2 represents 2% of the price yesterday."),
-              p("Under the tab 'Visualization', you can now see the", em("historical data"), "of the stocks that you can choose using the sidebar Panel.
+              p("Under the tab 'Visualization', you can see the", em("historical data"), "of the stocks that you can choose using the sidebar panel.
                 On the sidebar panel you will also be able to select the", em("training period"),". The training period will determine the historical data
                 that is given to the models for predictions. This application uses Prophet's Time Series Forecasting by Facebook and Holt Winters Exponential
                 Smoothing. To better understand how these models work, refer to the tabs 'Prophet' and 'Holt'."),
               p("The models are used to make predictions about what will happen to the price of the stock in the following days, weeks, or years.
-                The", em("prediction horizon") ,"on the sidebar Panel lets you choose how many days into the future you want the models to make the predictions for. 
-                For example, if you choose 180, you are going to obtain what each model predicts is going to happen to the share price of the stocks in the next
+                The", em("prediction horizon") ,"on the sidebar panel lets you choose how many days into the future you want the models to make the predictions for. 
+                For example, if you choose 180, you are going to see what each model predicts is going to happen to the share price of the stocks in the next
                 180 days (around 6 months)."),
               h2("How to Interpret Graphs"),
               p("For each of the selected stocks, you will be able to see the historical data for the selected training period, and the prediction. These will
                 be easily identifyable by the dashed vertical line on each of the plots, which denotes the end of the training data. That is, the part of the
-                graph to the left of this line refers to the historical data, and the part to the right of line refers to the predictions."),
+                graph to the left of this line refers to the historical data, and the part to the right of the line refers to the predictions."),
               width=5),
             column(
               p(strong("Historical Data"),"in the context of stocks, simply refers to the price of the stock in the past. The historical data
@@ -281,7 +280,7 @@ ui <- fluidPage(
                 This model extends to the prediction zone (to the right of the dashed vertical line) where it turns orange, which is simply the result of
                 'continuing' the line for the next 180 days. This line (the yellow-orange line) is what we call the model. The part that is in orange refers
                 prediction that the model makes for the following 180 days (first half of the year 2018), based on data from 2010. It is a prediction because
-                to create this line, we had only the training data available. That is, we knew nothing about what actually happened to the price after the end
+                to create this line, we only had the training data available. That is, we knew nothing about what actually happened to the price after the end
                 of the training period (marked by the horizontal dashed line). The shaded region around the orange section refers to the margin of error of 
                 the predictions. In purple, we have the actual Share Price of the stocks. To the left of the vertical dashed line, we can see how the actual data
                 and the model are very close together. This is because the model", em("uses"), "the historical data to draw the yellow line. To the right, we can
@@ -296,8 +295,8 @@ ui <- fluidPage(
             column(
               h1("Portfolio Tab"),
               p("Now you can move on to the Porfolio Tab to create your own virtual portfolio! To get started, you can choose the risk level you want your portfolio
-                to have which will pre-select some of the companies associated with that risk level, or you can manually choose companies that drew your attention when 
-                playing around on the analysis tab. Once you have selected the companies you wish to have in your portfolio, you will select how much money your want to
+                to have which will pre-select some of the companies associated with that risk level. You can also manually choose companies that drew your attention when 
+                playing around on the analysis tab. Once you have selected the companies you wish to have in your portfolio, you will select how much money you want to
                 invest (hypothetically, of course). Then, you will select what fraction of your money you wish to invest in each of the selected companies. You will also
                 have to specify, as you did on the Analysis tab, the training period you want to use for the predictions, and the prediction horizon. Then, use the sliders
                 to select the fraction of the investment amount that will be assigned to each of the companies you selected. By default, the amount is distributed equally 
@@ -308,7 +307,7 @@ ui <- fluidPage(
               tags$img(src="how_to2.png", width ="100%"),
               br(),
               p("The pie chart will show you the distribution of your portfolio among the selected companies. In the example shown above, I chose a High Risk Portfolio and
-                an initial investment amount of $100 USD. The training period and prediction horizon where the left at their default values."),
+                an initial investment amount of $100 USD. The training period and prediction horizon where left at their default values."),
               tags$img(src="how_to3.png", width ="100%"),
               br(),
               br(),
@@ -316,7 +315,7 @@ ui <- fluidPage(
                 values according to the Prophet model (left) and the Holt Winters model (right). The two bottom graphs show how each of the individual companies changed in 
                 value throughout the same period. This will allow you to see whether a given company was the main responsible for your profit or for losing money. On the 
                 Portfolio tab, you can hover over both of these graphs to see more detailed information about the values displayed (for example, the exact value of your 
-                portfolio at an exact date and the exact prediction at that date, or the name of the company that corresponds to a given color in two bottom graphs."),
+                portfolio at an exact date and the exact prediction at that date, or the name of the company that corresponds to a given color in two bottom graphs)."),
               br(),
               width = 8)
           ),
@@ -345,19 +344,19 @@ ui <- fluidPage(
                  historical data."),
               h2("Exponential Smoothing"),
               p("This technique builds on the weighted-average discussed above by assigning an exponential decay to the prices of the stock. What this means is that 
-                 we are going to care more about the more recent prices since we think that those have greater influence on tomorrow’s price than the prices further 
-                 into the past. The way we reduce the influence on previous values is exponential! This means that they are very quick at decreasing the influence of 
-                 past values."),
+                 we are going to care more about recent prices since we think that those have greater influence on tomorrow’s price than the prices further 
+                 into the past. The way we reduce the influence on previous values is by using a factor that follows an exponential decay! This means that they are 
+                 it becomes smaller very quick as we go further into the past."),
               h2("Holt Exponential Smoothing"),
-              p("his variation of the Exponential Smoothing allows for trends to be identified. If we simply reduced the influence of past data exponentially, a lot 
-                of value information of the past could be lost. Holt Exponential Smoothing helps us recover trends that happened in the past to compensate for the 
+              p("This variation of the Exponential Smoothing allows for trends to be identified. If we simply reduced the influence of past data exponentially, a lot 
+                of value information could be lost. Holt Exponential Smoothing helps us recover trends that happened in the past to compensate for the 
                 exponential decay that was associated with past data."),
               h2("Holt Winters Exponential Smoothing"),
               p("Finally, the Holt-Winters Exponential smoothing adds one more component to the algorithm: seasonality. In real life, prices are very likely influenced
                 by seasons (and here we do not talk about weather seasons necessarily, but rather different periods in the year which are associated with a periodic 
                 behavior, that is, a behavior that repeats itself). For example, people might receive bonuses by the end of the year, and so it is a great opportunity 
                 to use some of that money to make investments, or simply to spend it away. Either way, economic activity might be increased every winter due to this 
-                phenomenon (which, of course, is more nuanced than how it’s put here) which affects the stock market. The seasonal component of the algorithm aims to 
+                phenomenon  which affects the stock market (it is more complicated than that, but you get the idea). The seasonal component of the algorithm aims to 
                 capture this behavior and incorporate it into the model to make better predictions."),
               style="line-height:1.5;font-size:1.2em",
               width = 8)
@@ -371,7 +370,7 @@ ui <- fluidPage(
           column(width=2),
           column(
             h1('Prophet Time Series Model'),
-            p("Prophet uses a decomposable time series model with three main model components: growth, seasonality and holidays. On this tab, you will find a general
+            p("Prophet uses a decomposable time series model with three main components: growth, seasonality and holidays. On this tab, you will find a general
               overview of how Prophet works. More technical audiences will find this ",tags$a(href="https://peerj.com/preprints/3190.pdf","paper"),"more useful."),
             h2("Growth"),
             p("Prophet performs a regression using piecewise linear or logistic growth curve trend to detect changes in trends on the data. The image below was taken
